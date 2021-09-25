fc8bc6467aca16651a00d333dc8e16c110d53286
Taishi Kasuga <proxy0721@gmail.com>
2021-09-25 17:51:50 +0900

    fix

--- scripts/wait_pod_status.sh
+++ scripts/wait_pod_status.sh
@@ -4,11 +4,22 @@ set -euo pipefail
 
 readonly NAME=$1
 readonly STATUS=$2
+readonly MAX_ATTEMPTS=30
+readonly SLEEP_SEC=3
+readonly KUBE_CTX=kind-kind
+
+i=0
 
 while :
 do
+  if [[ $i -gt $MAX_ATTEMPTS ]]; then
+    echo "Max attempts exceeded: $i times"
+    kubectl --context=${KUBE_CTX} get pods
+    exit 1
+  fi
+
   sts=($(\
-    kubectl --context=kind-kind get pods -o json | \
+    kubectl --context=${KUBE_CTX} get pods -o json | \
     jq -r ".items[] | select(.metadata.name | contains(\"${NAME}\")) | .status.phase" | \
     sort | uniq))
 
@@ -17,5 +28,9 @@ do
   fi
 
   echo "Waiting for ${NAME} to be ${STATUS}..."
-  sleep 3
+  sleep ${SLEEP_SEC}
+
+  : $((++i))
 done
+
+exit 0
